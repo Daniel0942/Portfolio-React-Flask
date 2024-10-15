f9f65e14771b18f13fbb49770da30644a47fb4a3
DanielBomfim22 <danielufc131@gmail.com>
2024-10-15 17:37:02 -0300

    Quase pronto

--- front-end/src/components/topoDoSite/TopoDoSite.jsx
+++ front-end/src/components/topoDoSite/TopoDoSite.jsx
@@ -8,7 +8,7 @@ function TopoDoSite() {
         <section className={styles.topoDoSite}>
             <Interface estilo1="flex" estilo2="space-between" estilo3="margin-top">
                 <div className={styles.caixa_txt}>
-                    <h1>TRANSFORMANDO IDEIAS EM REALIDADE DIGITAL<span>.</span></h1>
+                    <h1>Desenvolvedor Full Stack<span>.</span></h1>
                     <p>Com uma abordagem focada na inovação e na eficiência, garanto que cada projeto seja adaptado às necessidades específicas de cada cliente, sempre com o objetivo de superar expectativas.</p>
                     <a href="https://wa.me/5579996746102" target="blank"><Button txt="Entre em contato"/></a>
                 </div>
