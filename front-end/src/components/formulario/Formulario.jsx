a28426f8685f66a7fc0f0661d9a7a61d99fe6694
DanielBomfim22 <danielufc131@gmail.com>
2024-10-15 08:42:13 -0300

    Form concluido

--- front-end/src/components/formulario/Formulario.jsx
+++ front-end/src/components/formulario/Formulario.jsx
@@ -1,20 +1,45 @@
 import Interface from "../../components/utilidades/Interface"
 import Button from "../../components/utilidades/Button"
 import styles from "./Formulario.module.css"
+import axios from "axios"
+import { useState } from "react"
 
 function Formulario() {
+    let [nome, setNome] = useState()
+    let [email, setEmail] = useState()
+    let [contato, setContato] = useState()
+    let [msg, setMsg] = useState()
+
     function Enviar(e) { //função de enviar o form
+        let novaMsg = {nome, email, contato, msg}
+
         e.preventDefault()
+        axios.post("https://localhost:5000/api", novaMsg)
+        .then(response => {
+            (console.log(response.data))
+            setNome("") //Limpar Formulario pós enviado
+            setEmail("") 
+            setContato("")
+            setMsg("")
+        })
+        .catch(error => (console.error("Ocorreu um erro ao enviar nova msg", error)))
     }
     return (
         <section className={styles.formulario}>
             <h2>FALA <span>COMIGO.</span></h2>
             <Interface>
                 <form onSubmit={Enviar}> {/* será aqui o evento */}
-                    <input type="text" placeholder="Seu Nome Completo" name="nome" required/>
-                    <input type="text" placeholder="Seu E-mail" name="email" required/>
-                    <input type="number" placeholder="Seu Contato" name="contato" required/>
-                    <textarea  placeholder="Sua Mensagem" name="msg"></textarea>
+                    <input type="text" placeholder="Seu Nome Completo" 
+                    onChange={(e)=> setNome(e.target.value)} required/>
+
+                    <input type="text" placeholder="Seu E-mail" 
+                    onChange={(e)=> setEmail(e.target.value)} required/>
+
+                    <input type="number" placeholder="Seu Contato" 
+                    onChange={(e)=> setContato(e.target.value)} required/>
+
+                    <textarea  placeholder="Sua Mensagem" 
+                    onChange={(e)=> setMsg(e.target.value)}></textarea>
                     <Button txt="Enviar"/>
                 </form>
             </Interface>
