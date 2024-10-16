7c925656e0afc6c066cd78ae0c822243356cbb53
DanielBomfim22 <danielufc131@gmail.com>
2024-10-16 10:48:35 -0300

    mudança

--- back-end/setup/__init__.py
+++ back-end/setup/__init__.py
@@ -2,15 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {
-    "origins": ["http://localhost:3000", "https://portfolio-react-flask-two.vercel.app/"],
-    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    "allow_headers": ["Content-Type", "Authorization"]
-}})
-
 CORS(app, resources={r"/api/*": {
     "origins": ["http://localhost:3000", "https://portfolio-react-flask-two.vercel.app"],
     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     "allow_headers": ["Content-Type", "Authorization"]
 }})
+
 from setup.controllers import default
