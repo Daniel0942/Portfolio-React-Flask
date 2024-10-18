1d8ce8bb4bc16080db06fc7b8905f5ea91d4a54d
DanielBomfim22 <danielufc131@gmail.com>
2024-10-18 20:27:33 -0300

    teste

--- back-end/setup/models/table.py
+++ back-end/setup/models/table.py
@@ -20,9 +20,23 @@ def criarTabela():
 def apagarDados(id):
     conectar = conexao()
     cursor = conectar.cursor()
-    cursor.execute("DELETE FROM users WHERE id = ? ", (id,))
+    try:
+        cursor.execute("DELETE FROM users WHERE id = ? ", (id,))
+        conectar.commit()
+    except Exception as e:
+        print(f"Ocorreu ao deletar: {e}")
+        conectar.rollback()
+    finally:
+        conectar.close()
+
+def inserirValores():
+    conectar = conexao()
+    cursor = conectar.cursor()
+    cursor.execute("INSERT INTO users (nome, email, contato, msg) VALUES (?, ?, ?, ?)", ("João", "Teste431gmail.com", 79993231, "Testando SQLite3 no flask"))
     conectar.commit()
     conectar.close()
 
 #criarTabela()
+#criarTabela()
 #apagarDados(1)
+inserirValores()
