42a33dd030df364deb596470ea30682bae336e3c
daniel bomfim <danielufc131@gmail.com>
2025-01-02 14:27:10 -0300

    final

--- front-end/src/components/projetos/Projetos.jsx
+++ front-end/src/components/projetos/Projetos.jsx
@@ -1,46 +1,88 @@
-import Interface from "../../components/utilidades/Interface"
-import styles from "./Projetos.module.css"
-import listaDeTarefasImg from '../../img/lista_de_tarefas.png'
-import Button from "../../components/utilidades/Button"
-import ceunoturnomistico from '../../img/ceu_noturno_mistico.png'
-import Ecommerce from '../../img/E-commerce.png'
+import Interface from "../../components/utilidades/Interface";
+import styles from "./Projetos.module.css";
+import listaDeTarefasImg from "../../img/lista_de_tarefas.png";
+import Button from "../../components/utilidades/Button";
+import ceunoturnomistico from "../../img/ceu_noturno_mistico.png";
+import Ecommerce from "../../img/E-commerce.png";
+import promolink from "../../img/promolink.png";
 
 function Projetos() {
     return (
-        <section className={styles.projetos} id="projetos">
-            <h2>MEUS <span>PROJETOS.</span></h2>
-            <Interface estilo1="flex">
+        <section className={styles.projetos} id='projetos'>
+            <h2>
+                MEUS <span>PROJETOS.</span>
+            </h2>
+            <Interface estilo1='flex'>
                 <div className={styles.container}>
                     <div
-                    className={`${styles.projeto_box} ${styles.primeiro}`}
-                    style={{ backgroundImage: `url(${listaDeTarefasImg})`}}>
+                        className={`${styles.projeto_box} ${styles.primeiro}`}
+                        style={{ backgroundImage: `url(${listaDeTarefasImg})` }}
+                    >
                         <div className={styles.overlay}>Projeto 1</div>
                     </div>
-                    <a href="https://github.com/Daniel0942/Lista_de_tarefas_React.js" target="blank"><Button txt="Repositório"/></a>
-                    <a href="https://lista-de-tarefas-react-js-seven.vercel.app/" target="blank"><Button txt="Site"/></a>
+                    <a
+                        href='https://github.com/Daniel0942/Lista_de_tarefas_React.js'
+                        target='blank'
+                    >
+                        <Button txt='Repositório' />
+                    </a>
+                    <a
+                        href='https://lista-de-tarefas-react-js-seven.vercel.app/'
+                        target='blank'
+                    >
+                        <Button txt='Site' />
+                    </a>
                 </div>
 
                 <div className={styles.container}>
                     <div
-                    className={`${styles.projeto_box} ${styles.segundo}`}
-                    style={{ backgroundImage: `url(${ceunoturnomistico})`}}>
+                        className={`${styles.projeto_box} ${styles.segundo}`}
+                        style={{ backgroundImage: `url(${ceunoturnomistico})` }}
+                    >
                         <div className={styles.overlay}>Projeto 2</div>
                     </div>
-                    <a href="https://github.com/Daniel0942/Ceu_Noturno_Mistico" target="blank"><Button txt="Repositório"/></a>
-                    <a href="#" target="blank"><Button txt="Site"/></a>
+                    <a
+                        href='https://github.com/Daniel0942/Ceu_Noturno_Mistico'
+                        target='blank'
+                    >
+                        <Button txt='Repositório' />
+                    </a>
+                    <a href='#' target='blank'>
+                        <Button txt='Site' />
+                    </a>
                 </div>
 
                 <div className={styles.container}>
                     <div
-                    className={`${styles.projeto_box} ${styles.terceiro}`}
-                    style={{ backgroundImage: `url(${Ecommerce})`}}>
+                        className={`${styles.projeto_box} ${styles.terceiro}`}
+                        style={{ backgroundImage: `url(${Ecommerce})` }}
+                    >
                         <div className={styles.overlay}>Projeto 3</div>
                     </div>
-                    <a href="https://github.com/Daniel0942/E-commerce" target="blank"><Button txt="Repositório"/></a>
-                    <a href="#" target="blank"><Button txt="Site"/></a>
+                    <a href='https://github.com/Daniel0942/E-commerce' target='blank'>
+                        <Button txt='Repositório' />
+                    </a>
+                    <a href='#' target='blank'>
+                        <Button txt='Site' />
+                    </a>
+                </div>
+
+                <div className={styles.container}>
+                    <div
+                        className={`${styles.projeto_box} ${styles.quarto}`}
+                        style={{ backgroundImage: `url(${promolink})` }}
+                    >
+                        <div className={styles.overlay}>Projeto 4</div>
+                    </div>
+                    <a href='https://github.com/Daniel0942/PromoLink' target='blank'>
+                        <Button txt='Repositório' />
+                    </a>
+                    <a href='https://promo-link.vercel.app/' target='blank'>
+                        <Button txt='Site' />
+                    </a>
                 </div>
             </Interface>
         </section>
-    )
+    );
 }
-export default Projetos
+export default Projetos;
